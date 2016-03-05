90d818b80b8548128abb182f6ee0707730c09f2e
jorge jardines <gardenunez@gmail.com>
2016-03-05 06:44:11 +0100

    add logic to courses controller

--- my_first_app/app/models/coursera.rb
+++ my_first_app/app/models/coursera.rb
@@ -1,11 +1,11 @@
-class coursera
-	include HTTParty
-	
-	base_uri 'https://api.coursera.org/api/catalog.v1/courses'
-	default_params fields: "smallIcon,shortDescription", q: "search"
-	format: json
-	
-	def self.for term
-		get("", query: {query : term })["elements"]
-	end
+class Coursera
+  include HTTParty
+
+  base_uri 'https://api.coursera.org/api/catalog.v1/courses'
+  default_params fields: "smallIcon,shortDescription", q: "search"
+  format :json
+
+  def self.for term
+    get("", query: { query: term})["elements"]
+  end
 end
